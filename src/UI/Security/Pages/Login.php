35b88f9dc6f8b21ef100c2152f378f81ede32b97
Bruce <bruce@mediagone.fr>
2021-01-09 22:35:26 +0100

    Add "Login" page

---
+++ src/UI/Security/Pages/Login.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+
+namespace App\UI\Security\Pages;
+
+use App\UI\Backend\Partials\BackendHeader;
+use App\UI\Shared\Partials\PageHeader;
+use App\UI\Shared\Services\ControllerFlashes;
+use App\UI\Shared\Services\ControllerResponses;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AuthenticationServiceException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+/**
+ * @Route("/login", name="security_login", methods={"GET","POST"})
+ */
+final class Login
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+
+    private AuthenticationUtils $auth;
+
+    private ControllerFlashes $flashes;
+
+
+
+    //========================================================================================================
+    // Constructor
+    //========================================================================================================
+
+    public function __construct(AuthenticationUtils $auth, ControllerFlashes $flashes)
+    {
+        $this->auth = $auth;
+        $this->flashes = $flashes;
+    }
+
+
+
+    //========================================================================================================
+    // Action
+    //========================================================================================================
+
+    public function __invoke(?UserInterface $user, ControllerResponses $responses, AuthenticationUtils $authenticationUtils) : Response
+    {
+        $error = $this->auth->getLastAuthenticationError();
+        
+        if ($user !== null) {
+            return $responses->seeOtherRedirection('backend_dashboard');
+        }
+        
+        $this->handleAuthenticationError();
+        
+        return $responses->template('Security/Pages/Login.twig', [
+            'HEADER' => new PageHeader(null),
+            'MENU' => null,
+            'FOOTER' => new BackendHeader(),
+        ]);
+    }
+
+
+
+    //========================================================================================================
+    // Helpers
+    //========================================================================================================
+
+    private function handleAuthenticationError()
+    {
+        $error = $this->auth->getLastAuthenticationError();
+        
+        if ($error instanceof BadCredentialsException || $error instanceof AuthenticationServiceException) {
+            $this->flashes->warning('login.flashes.error_credentials_html', 'frontend');
+        }
+        elseif ($error instanceof InvalidCsrfTokenException) {
+            $this->flashes->warning('login.flashes.error_csrf_html', 'frontend');
+        }
+        elseif ($error) {
+            $this->flashes->warning('login.flashes.error_html', 'frontend');
+        }
+    }
+    
+    
+    
+}
--- src/UI/Security/Services/SecurityUserAuthenticator.php
+++ src/UI/Security/Services/SecurityUserAuthenticator.php
@@ -72,8 +72,8 @@ final class SecurityUserAuthenticator extends AbstractFormLoginAuthenticator
     public function getCredentials(Request $request) : array
     {
         $credentials = [
-            'email' => $request->request->get('_username'),
-            'password' => $request->request->get('_password'),
+            'email' => $request->request->get('username'),
+            'password' => $request->request->get('password'),
             'csrf_token' => $request->request->get('_csrf_token'),
         ];
         
