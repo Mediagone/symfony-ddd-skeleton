f6d5668e7e724055665a9ba6f7ac4ed582daf33c
Bruce <bruce@mediagone.fr>
2021-01-14 23:07:17 +0100

    Add "Dashboard" page in Backend

---
+++ src/UI/Backend/Actions/DashboardAction.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\UI\Backend\Actions;
+
+use App\UI\Backend\Views\Pages\Dashboard;
+use App\UI\Shared\Services\ControllerResponses;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/admin", name="backend_dashboard", methods={"GET"})
+ */
+final class DashboardAction
+{
+    //========================================================================================================
+    // Action
+    //========================================================================================================
+    
+    public function __invoke(ControllerResponses $responses) : Response
+    {
+        return $responses->template('Backend/Views/Pages/Dashboard.twig', [
+            'MODEL' => new Dashboard(),
+        ]);
+    }
+    
+    
+    
+}
