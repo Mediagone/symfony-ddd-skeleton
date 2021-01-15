67a2031ef130f06a7f002ab0d45ecb51a15b7789
Bruce <bruce@mediagone.fr>
2021-01-15 14:19:02 +0100

    Add "AccountList" page in Backend

---
+++ src/UI/Backend/Views/Pages/AccountList.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\UI\Backend\Views\Pages;
+
+use App\Domain\Core\Account\Account;
+
+
+final class AccountList
+{
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private array $accounts;
+    
+    public function getAccounts() : array
+    {
+        return $this->accounts;
+    }
+    
+    
+    
+    //========================================================================================================
+    // Constructor
+    //========================================================================================================
+
+
+    public function __construct(array $accounts)
+    {
+        $this->accounts = (static fn(Account... $accounts) => $accounts)(...$accounts);
+    }
+    
+
+
+}
