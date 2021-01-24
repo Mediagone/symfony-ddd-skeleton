b1f29fe3d52344261a00ddc503db47dadd5d9d89
Bruce <bruce@mediagone.fr>
2021-01-24 01:40:10 +0100

    Form styles in default theme

--- src/UI/Shared/Themes/Default/Styles.tailwind.config.js
+++ src/UI/Shared/Themes/Default/Styles.tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     plugins: [
         require('tailwindcss'),
+        require('@tailwindcss/forms'),
         require('autoprefixer'),
     ],
     purge: {
